4920ce86d9032d82506be6978ca64fefb44ed714
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-01-12 22:48:24 +0600

    all users added

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ async function run() {
             const query = {email: email};
             const result = await usersCollection.findOne(query);
             res.send(result)
+        });
+
+        app.get('/users', async (req, res ) =>{
+            const query = {};
+            const result = await usersCollection.find(query).toArray();
+            res.send(result);
+
         })
 
 
