16b484a9c342d02121a1aaca48a16f31cd056eca
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-01-13 02:38:21 +0600

    update admin added

--- index.js
+++ index.js
@@ -77,6 +77,28 @@ async function run() {
             const result = await usersCollection.find(query).toArray();
             res.send(result);
 
+        });
+
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({user })
+        });
+
+        app.put('/users/admin/:id',  async (req, res) => {
+
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+
         })
 
 
